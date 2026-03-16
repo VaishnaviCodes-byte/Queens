28001f69b9f1a2fc6697fcc3c8c02531bd25fb27
VaishnaviCodes-byte <vaishnavibaragali@gmail.com>
2026-03-16 23:15:40 +0530

    update sever.js

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(5000, () =>
-      console.log("Server running on port 5000")
-    );
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Queens Backend API is Live 👑");
+});
